d8de5bdd3884693d3a8608073919ae90d53607f6
zeppfreak <zeppfreak@gmail.com>
2021-08-29 09:03:01 +0900

    add model.py

---
+++ src/logic/model.py
@@ -0,0 +1,15 @@
+import tensorflow as tf
+from tensorflow import keras
+
+def create_model(model_id):
+    if model_id == "multiinput_multioutput":
+        input_A = keras.layers.Input(shape=(5,), name="wide_input")
+        input_B = keras.layers.Input(shape=(6,), name="deep_input")
+        hidden1 = keras.layers.Dense(30, activation="relu")(input_B)
+        hidden2 = keras.layers.Dense(30, activation="relu")(hidden1)
+        concat = keras.layers.concatenate([input_A, hidden2])
+        output = keras.layers.Dense(1, name="output")(concat)
+        aux_output = keras.layers.Dense(1, name="aux_output")(hidden2)
+        model = keras.Model(inputs=[input_A, input_B], outputs=[output, aux_output])
+
+    return model
