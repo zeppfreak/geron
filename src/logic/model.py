bec479483c2b4be74fa7ba603d50e15bd3dfacd4
zeppfreak <zeppfreak@gmail.com>
2021-08-29 13:15:01 +0900

    refactor main.ipynb

--- src/logic/model.py
+++ src/logic/model.py
@@ -2,7 +2,13 @@ import tensorflow as tf
 from tensorflow import keras
 
 def create_model(model_id):
-    if model_id == "multiinput_multioutput":
+    if model_id == "singleinput_singleoutput":
+        input   = keras.layers.Input(shape=(8,), name="deep_input")
+        hidden1 = keras.layers.Dense(30, activation="relu")(input)
+        hidden2 = keras.layers.Dense(30, activation="relu")(hidden1)
+        output = keras.layers.Dense(1, name="output")(hidden2)
+        model = keras.Model(inputs=[input], outputs=[output])
+    elif model_id == "multiinput_multioutput":
         input_A = keras.layers.Input(shape=(5,), name="wide_input")
         input_B = keras.layers.Input(shape=(6,), name="deep_input")
         hidden1 = keras.layers.Dense(30, activation="relu")(input_B)
