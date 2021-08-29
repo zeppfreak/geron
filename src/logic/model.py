f5c9f3f7726dab28cd8ac377db6eef8333206714
zeppfreak <zeppfreak@gmail.com>
2021-08-29 16:24:14 +0900

    add model_id=multiinput_singleoutput

--- src/logic/model.py
+++ src/logic/model.py
@@ -8,6 +8,14 @@ def create_model(model_id):
         hidden2 = keras.layers.Dense(30, activation="relu")(hidden1)
         output = keras.layers.Dense(1, name="output")(hidden2)
         model = keras.Model(inputs=[input], outputs=[output])
+    elif model_id == "multiinput_singleoutput":
+        input_A = keras.layers.Input(shape=(5,), name="wide_input")
+        input_B = keras.layers.Input(shape=(6,), name="deep_input")
+        hidden1 = keras.layers.Dense(30, activation="relu")(input_B)
+        hidden2 = keras.layers.Dense(30, activation="relu")(hidden1)
+        concat = keras.layers.concatenate([input_A, hidden2])
+        output = keras.layers.Dense(1, name="output")(concat)
+        model = keras.Model(inputs=[input_A, input_B], outputs=[output]) 
     elif model_id == "multiinput_multioutput":
         input_A = keras.layers.Input(shape=(5,), name="wide_input")
         input_B = keras.layers.Input(shape=(6,), name="deep_input")
